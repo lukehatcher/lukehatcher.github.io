fb640ce610c5121433b8aeafc9e9b19fa83734f0
Luke Hatcher <lukehatcher98@gmail.com>
2021-01-22 14:04:05 -0800

    added file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'file-loader',
+      },
     ],
   },
   resolve: {
